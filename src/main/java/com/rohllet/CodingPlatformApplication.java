dae586529d7aa7a38f2e42ca30013ebbadfb90a5
rohandol112 <rohan45321dol@gmail.com>
2025-11-30 14:13:38 +0530

    setup done

---
+++ src/main/java/com/rohllet/CodingPlatformApplication.java
@@ -0,0 +1,11 @@
+﻿package com.rohllet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CodingPlatformApplication.class, args);
+    }
+}
---
+++ src/main/java/org/example/CodingPlatformApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CodingPlatformApplication.class, args);
+    }
+}
